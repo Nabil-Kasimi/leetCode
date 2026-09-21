cc2c299f21d2eb00b9482017d5544da6f28873f3
Nabil-Kasimi <kaisenkaisen100@gmail.com>
2026-09-21 12:46:12 -0400

    Time: 0 ms (100%), Space: 42.1 MB (36.23%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 3)
+            return n;
+
+        int p1 = 1;
+        int p2 = 2;
+
+        int ans = 0;
+        for(int i = 3 ; i <= n ; i++)
+        {
+            ans= p1+p2;
+            p1 = p2;
+            p2 = ans;
+        }
+        return ans;
+    }
+}
