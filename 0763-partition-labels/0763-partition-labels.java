3ef32f49499f70a2b2c9503f6b119ca24361feb9
Nabil-Kasimi <kaisenkaisen100@gmail.com>
2026-09-22 13:46:51 -0400

    Time: 5 ms (81.15%), Space: 43.4 MB (79.89%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> lst = new ArrayList<>();
+        int reco[] = new int[26];
+
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            reco[c-'a']= i; 
+        }
+
+
+        int l = reco[s.charAt(0)-'a'];
+        int cn = 0;
+
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            cn++;
+            char c = s.charAt(i);
+            if(reco[c-'a'] > l)
+                l =  reco[c-'a'];
+            if(i == l)
+            {
+                lst.add(cn);
+                cn = 0;
+            }
+        }
+
+       return lst;
+    }
+}
